3798351a2299e6bd3126a05dc30f1082a0939383
Cao Dinh Nghia <25023339@vnu.edu.vn>
2026-05-14 20:18:16 +0700

    bai7

---
+++ bai7/src/main/java/org/example/BadCode.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class BadCode {
+    /**
+     * Phương thức kiểm tra đã được sửa lỗi định dạng.
+     */
+    public void testMethod() {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("Lỗi đã được sửa!");
+        }
+    }
+}
